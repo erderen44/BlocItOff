01ca39f93d42d093ebcca8053d77fe06ae81c589
Jonathan <jonathan_whitfield@hotmail.com>
2017-04-13 21:56:28 +0800

    added some unstaged files from other directories

---
+++ dist/scripts/controllers/homeCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+    function homeCtrl($scope, $firebaseArray) {
+    }
+    
+    angular
+        .module('blocItOff')
+        .controller('homeCtrl', ['$scope', '$firebaseArray',homeCtrl]);
+})();
