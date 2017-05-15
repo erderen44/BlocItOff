998e048eaff167f8bd4fce61f5de6a84449240ae
Jonathan <jonathan_whitfield@hotmail.com>
2017-05-16 00:05:10 +0800

    Added delete functionality from mentor meeting

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -2,12 +2,48 @@
     function homeCtrl($scope, $firebaseArray) {
         var ref = firebase.database().ref()
         $scope.tasks = $firebaseArray(ref);
-//         var jobs = "Feed the Fish","Water the Plants","Mop the Floor","Do the Dishes"];
         $scope.tasks.$add({
-            task: "Clean"
+            task: "Tidy Up", 
+            age: 2,
+            completed: "false"
+            
+//            {
+//                text: $scope.newTaskText,
+//                created: Firebase.ServerValue.TIMESTAMP,
+//                date: $scope.newTaskDate.getTime(),
+//                status: 'active',
+//                expired: false,
+//                priority: $scope.newTaskPriority
+//            }
         });
+        var item = $scope.tasks;
+        console.log(item);
+        console.log(item[2]);
+        $scope.tasks.$remove( "Feed the Fish"  
+            );
+        
+        $scope.deleteTask = function (id){
+            $scope.tasks.$remove(id)
+        }
+//         $scope.removeTodo = function(todo) {
+//        for (i=0; i<$scope.tasks.length; i++){
+//                if ($scope.tasks.$indexFor[i].task == "Feed the Fish"){
+//                    $scope.tasks.$remove($scope.tasks.$indexFor[i][task]);
+//                }
+//        } 
+//        $scope.tasks = $firebaseArray(ref);
+//        var anItem = $scope.tasks[2];
+//        list.$remove(item).then(function(ref) {
+//            ref.key === item.$id; // true
+//            });     
+//    $scope.todos.$remove(todo); 
+//  };
+//        $scope.hideOldTasks = function(oldTask){
+//            $scope.tasks.$remove( "Do the Dishes"  
+//            );
+//        };
+//        $scope.hideOldTasks("Do the Dishes");
         };
-    
     angular
         .module('blocItOff')
         .controller('homeCtrl', ['$scope', '$firebaseArray', homeCtrl]);
