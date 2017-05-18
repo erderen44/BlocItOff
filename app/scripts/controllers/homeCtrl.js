c156afd8d23e032c1de10a4a63be39e9f9bb427d
Jonathan <jonathan_whitfield@hotmail.com>
2017-05-18 21:23:56 +0800

    Added the ability to add new tasks to database

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -2,11 +2,15 @@
     function homeCtrl($scope, $firebaseArray) {
         var ref = firebase.database().ref()
         $scope.tasks = $firebaseArray(ref);
-        $scope.tasks.$add({
-            task: "Tidy Up", 
-            age: 2,
-            completed: "false"
-            
+        $scope.addATask = function () {
+            console.log("Is this working? " + $scope.anAddedTask);
+            $scope.tasks.$add({
+                task: $scope.anAddedTask, 
+                age: 2,
+                completed: "false"
+            });
+        };                  
+                
 //            {
 //                text: $scope.newTaskText,
 //                created: Firebase.ServerValue.TIMESTAMP,
@@ -15,34 +19,18 @@
 //                expired: false,
 //                priority: $scope.newTaskPriority
 //            }
-        });
-        var item = $scope.tasks;
-        console.log(item);
-        console.log(item[2]);
-        $scope.tasks.$remove( "Feed the Fish"  
-            );
         
-        $scope.deleteTask = function (id){
-            $scope.tasks.$remove(id)
-        }
+
+
+            $scope.deleteTask = function (id){
+                $scope.tasks.$remove(id)
+            }
 //         $scope.removeTodo = function(todo) {
 //        for (i=0; i<$scope.tasks.length; i++){
 //                if ($scope.tasks.$indexFor[i].task == "Feed the Fish"){
 //                    $scope.tasks.$remove($scope.tasks.$indexFor[i][task]);
 //                }
-//        } 
-//        $scope.tasks = $firebaseArray(ref);
-//        var anItem = $scope.tasks[2];
-//        list.$remove(item).then(function(ref) {
-//            ref.key === item.$id; // true
-//            });     
-//    $scope.todos.$remove(todo); 
-//  };
-//        $scope.hideOldTasks = function(oldTask){
-//            $scope.tasks.$remove( "Do the Dishes"  
-//            );
-//        };
-//        $scope.hideOldTasks("Do the Dishes");
+
         };
     angular
         .module('blocItOff')
